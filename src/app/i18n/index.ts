230d13b1d76dd270bd31373f78ac66061180cae8
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-04-11 14:06:28 -0500

    feat: add handleErrors

---
+++ src/app/i18n/index.ts
@@ -0,0 +1,22 @@
+import translationEn from "@/app/i18n/locales/en/translation.json";
+import translationEs from "@/app/i18n/locales/es/translation.json";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+	resources: {
+		en: {
+			translation: translationEn,
+		},
+		es: {
+			translation: translationEs,
+		},
+	},
+	lng: "en",
+	fallbackLng: "en",
+	interpolation: {
+		escapeValue: false,
+	},
+});
+
+export default i18n;
