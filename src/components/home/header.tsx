78ab3f30577cc1a80d7a084361ed3f7b9751c586
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-04-11 17:09:14 -0500

    feat: mobile search

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,40 +1,44 @@
-import { useAuth } from "@/modules/auth/hooks/use-auth"
-import { Dispatch } from "react"
-import { SidebarTrigger } from "../ui/sidebar"
-import { Breadcrumb } from "../ui/breadcrumb"
-import { Separator } from "../ui/separator"
+import { useAuth } from "@/modules/auth/hooks/use-auth";
+import { Dispatch } from "react";
+import { SidebarTrigger } from "../ui/sidebar";
+import { Breadcrumb, BreadcrumbList } from "../ui/breadcrumb";
+import { Separator } from "../ui/separator";
+import PageTitle from "../sidebar/page-title";
 
 interface HeaderProps {
-  state: boolean
-  setState: Dispatch<React.SetStateAction<boolean>>
+  state: boolean;
+  setState: Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const Header = ({state, setState}: HeaderProps) => {
+export const Header = ({ state, setState }: HeaderProps) => {
   //const {signOut} = useUserStore()
-  const {signOut} = useAuth()
+  const { signOut } = useAuth();
 
   const handleLogout = async () => {
-    await signOut()
-  }
-
+    await signOut();
+  };
 
   return (
     <header className="flex items-center gap-2 border-b ">
       <div className="flex items-center gap-2 px-4 w-full">
-      <SidebarTrigger />
-      <Separator orientation="vertical" />
-      <Breadcrumb className="flex items-center justify-between w-full">
-        <h1 className="text-2xl font-bold">Home Page</h1>
-        <div className="flex items-center gap-2">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-            Dashboard
-          </button>
-          <button className="bg-red-500 text-white px-4 py-2 rounded-md">
-            Logout
-          </button>
-        </div>
-      </Breadcrumb>
+        <SidebarTrigger />
+        <Separator orientation="vertical" />
+        <Breadcrumb className="w=full">
+          <BreadcrumbList className="flex items-center justify-between w-full">
+            <h1 className="text-lg font-bold truncate">
+              <PageTitle />
+            </h1>
+            <div className="flex items-center gap-2">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                Dashboard
+              </button>
+              <button className="bg-red-500 text-white px-4 py-2 rounded-md">
+                Logout
+              </button>
+            </div>
+          </BreadcrumbList>
+        </Breadcrumb>
       </div>
     </header>
-  )
-} 
+  );
+};
---
+++ src/components/home/mobile-search.tsx
@@ -0,0 +1,51 @@
+
+import { SearchIcon } from "lucide-react";
+import { Dispatch, SetStateAction } from "react";
+import { useTranslation } from "react-i18next";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+interface MobileSearchProps {
+  isSearchOpen: boolean;
+  setIsSearchOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const MobileSearch = ({
+  isSearchOpen,
+  setIsSearchOpen,
+}: MobileSearchProps) => {
+  const { t } = useTranslation();
+  return (
+    <Sheet open={isSearchOpen} onOpenChange={setIsSearchOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="md:hidden"
+          onClick={() => setIsSearchOpen(true)}
+        >
+          <SearchIcon className="h-4 w-4" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="top" className="h-auto max-h-[40vh] pt-10">
+        <div className="relative w-full">
+          <Input
+            autoFocus
+            placeholder={t("dashboard.search")}
+            className="pl-9 h-10 w-full"
+          />
+          <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-2 top-1/2 transform -translate-y-1/2"
+            onClick={() => setIsSearchOpen(false)}
+          >
+           
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
