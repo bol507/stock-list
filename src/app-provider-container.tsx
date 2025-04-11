82e43e3650b0eee050cf6280dd0a8c29a37c54c2
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-04-11 13:45:55 -0500

    feat: i18n

--- src/app-provider-container.tsx
+++ src/app-provider-container.tsx
@@ -1,14 +1,18 @@
 import App from "./App";
 import { ThemeProvider } from "./app/hooks/theme-context";
 import AppRouter from "./app/router/app-router";
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/app/i18n";
 
 const AppProviderContainer = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AppRouter>
-        <App />
+        <I18nextProvider i18n={i18n} defaultNS={"translation"}>
+          <App />
+        </I18nextProvider>
       </AppRouter>
-    </ThemeProvider>  
+    </ThemeProvider>
   );
 };
 
